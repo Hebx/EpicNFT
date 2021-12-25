b0fa2392a6269d140f8ebec03659bead2727c06b
Ihab Hormi <ihormi@lab3r8s1.42abudhabi.ae>
2021-12-25 07:30:24 +0400

    Verified Contract on Etherscan, Deploy

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,16 +1,17 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 import EpicNFT from "./utils/EpicNFT.json";
-import {Spinner} from "@chakra-ui/react"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'LordHeb';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // const OPENSEA_LINK = '';
 // const TOTAL_MINT_COUNT = 50;
-const CONTRACT_ADDRESS = "0x0876Cb5C2F5B23C8dadcb65694d4eE908ceB87b0";
+const CONTRACT_ADDRESS = "0xe090F86Ec818ac271690396FBC44F6088a139dbe";
 
 const App = () => {
   // State variable we use to store our user's public wallet
@@ -39,6 +40,28 @@ const App = () => {
       console.log("No authorized accounts found");
     }
   }
+
+  const checkIfOnCorrectNetwork = async () => {
+    try {
+      const {ethereum} = window;
+      if (ethereum) {
+        let chainID = await ethereum.request({
+          method: 'eth_chainId'
+        });
+        console.log("connected to chain " + chainID);
+        // string, hex code of the chainID of the Rinkeby network
+        const rinkebyChainID = "0x4";
+        if (chainID !== rinkebyChainID) {
+          alert("You are not connected to the Rinkeby Test Network.");
+        }
+      } else {
+        console.log("We have no ethereum object");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
@@ -53,15 +76,6 @@ const App = () => {
       setCurrentUser(accounts[0]);
       // Setup Listener for a new user that connect their wallet for the fist time
       eventListener();
-      let chainID = await ethereum.request({
-        method: 'eth_chainId'
-      });
-      console.log("connected to chain " + chainID);
-      // string, hex code of the chainID of the Rinkeby network
-      const rinkebyChainID = "0x4";
-      if (chainID !== rinkebyChainID) {
-        alert("You are not connected to the Rinkeby Test Network.");
-      }
     } catch (error) {
       console.log(error);
     }
@@ -76,6 +90,7 @@ const App = () => {
         // capture our smart contract emitted event same as webhooks
         connectedContract.on("NewEpicNFTMinted", (from, tokenID) => {
           console.log(from, tokenID.toNumber());
+          setIsLoading(false);
           alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenID.toNumber()}`)
         });
         console.log("Setup event Listener");
@@ -100,6 +115,7 @@ const App = () => {
       console.log("Mining ... Please Wait!");
       await nftMint.wait();
       console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftMint.hash}`);
+      setIsLoading(true);
     } else {
       console.log("ethereum object not found");
     }
@@ -111,6 +127,7 @@ const App = () => {
   // run our function when the page loads
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkIfOnCorrectNetwork();
   }, []);
 
   // Render Methods
@@ -119,10 +136,8 @@ const App = () => {
       Connect to Wallet
     </button>
   );
-  const spinner = <Spinner/>;
 
   const renderMintUI = () => (
-    isLoading ? spinner :
     <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
        Mint NFT
     </button>
@@ -137,6 +152,23 @@ const App = () => {
             Each unique. Each beautiful. Discover your NFT today.
           </p>
           {currentUser === "" ? renderNotConnectedContainer() : renderMintUI()}
+        </div>
+        <div
+          style={{
+            display: isLoading ? "flex" : "none",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Loader
+            type="Puff"
+            color="#60c657"
+            height={50}
+            width={50}
+            // timeout={3000} //3 secs
+          />
+          <p style={{ color: "#fff" }}>Minting, please wait.</p>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
